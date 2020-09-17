36b1a154d0aaa4fe04c175e2aa743e9557e3e647
Evan Soikie <evan.soikie@shopify.com>
2020-09-17 11:53:54 -0300

    Update drilldown.js

--- docs/drilldown.js
+++ docs/drilldown.js
@@ -174,35 +174,4 @@ function drilldown(myparams) {
 
     document.getElementById(divid).innerHTML = code
 };
-$(function() {
-    $('.tablesorter-childRow1 td').hide();
-    $('.tablesorter-childRow2 td').hide();
-    $('.tablesorter-childRow3 td').hide();
-    $('.tablesorter-childRow4 td').hide();
-    $(".tablesorter")
-        .tablesorter({
-            cssChildRow: "tablesorter-childRow1"
-        })
-    $('.tablesorter').delegate('.toggle0', 'click', function() {
-        $(this).closest('tr').nextUntil('tr:not(.tablesorter-childRow1').find('td').toggle();
-        $('.tablesorter-childRow2 td').hide();
-        $('.tablesorter-childRow3 td').hide();
-        $('.tablesorter-childRow4 td').hide();
-        return false;
-    });
-    $('.tablesorter').delegate('.toggle1', 'click', function() {
-        $(this).closest('tr').nextUntil('tr:not(.tablesorter-childRow2)').find('td').toggle();
-        $('.tablesorter-childRow3 td').hide();
-        $('.tablesorter-childRow4 td').hide();
-        return false;
-    });
-    $('.tablesorter').delegate('.toggle2', 'click', function() {
-        $(this).closest('tr').nextUntil('tr:not(.tablesorter-childRow3)').find('td').toggle();
-        $('.tablesorter-childRow4 td').hide();
-        return false;
-    });
-    $('.tablesorter').delegate('.toggle3', 'click', function() {
-        $(this).closest('tr').nextUntil('tr:not(.tablesorter-childRow4)').find('td').toggle();
-        return false;
-    });
-});
+
