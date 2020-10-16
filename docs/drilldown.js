255608889e1ea0194eb070f38f2273b2b22efc08
Evan Soikie <evan.soikie@shopify.com>
2020-10-15 23:06:29 -0300

    Update drilldown.js

--- docs/drilldown.js
+++ docs/drilldown.js
@@ -187,8 +187,8 @@ function collapse() {
         })
     $('.tablesorter').delegate('.toggle0', 'click', function() {
  //       $(this).closest('tr').nextUntil('tr:not(.tablesorter-childRow1').find('td').toggle();
-        cells = $(this).closest('tr').get(0);
-        cells = cells.getElementsByTagName("td");
+        cells = $(this).closest('tr').get;
+        cells = cells[0].getElementsByTagName("td");
         for(var c = 0; c < cells.length; c++){
           cells[c].style.display = document.getElementsByClassName('col' + (c+1))[0].style.display;
         };
